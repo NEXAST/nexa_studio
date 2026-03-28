e69f8bef09543f54d631bb07d9cd072ec6bef121
nexa_studio <jovicickristian10@gmail.com>
2026-03-28 22:10:37 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,3 @@
-// Init Animations
-AOS.init({
-    duration: 1000,
-    once: false,
-    mirror: true
-});
-
-// WhatsApp Function
 function sendWA() {
     const msg = document.getElementById('msg').value;
     const phone = "38765959096";
@@ -16,14 +8,8 @@ function sendWA() {
     }
 }
 
-// Navbar Scroll Effect
+// Navigacija menja providnost na skrol
 window.addEventListener('scroll', () => {
     const nav = document.querySelector('nav');
-    if (window.scrollY > 50) {
-        nav.style.background = 'rgba(0,0,0,0.95)';
-        nav.style.padding = '20px 10%';
-    } else {
-        nav.style.background = 'transparent';
-        nav.style.padding = '40px 10%';
-    }
+    nav.style.background = window.scrollY > 50 ? 'rgba(0,0,0,0.95)' : 'rgba(0,0,0,0.8)';
 });
